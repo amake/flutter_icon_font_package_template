29242083c35402d4e744c569c98fe33f9d04bb67
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-06-07 00:17:33 +0900

    Generate assets, include in package

---
+++ lib/custom_font.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class CustomFontIcons {
+  CustomFontIcons._();
+
+  static const IconData uE000 = IconData(0xE000, fontFamily: 'CustomFont', fontPackage: 'flutter_icon_font_package_template');
+}
+
