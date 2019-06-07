00ae4f903190d0afa6e096666be2447c56b248bf
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-06-07 22:46:24 +0900

    Replace default test

--- lib/flutter_icon_font_package_template.dart
+++
@@ -1,7 +0,0 @@
-library flutter_icon_font_package_template;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ test/custom_font_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_icon_font_package_template/custom_font.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('icon data exists', () {
+    expect(CustomFontIcons.uE000, isNotNull);
+  });
+}
